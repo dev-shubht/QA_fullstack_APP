ee20dcceca1eaa5736a21fcf421f8cf1555bb4fd
dev-shubht <101338499+dev-shubht@users.noreply.github.com>
2025-10-28 12:12:02 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://team-q-a.onrender.com', 
+  origin: 'https://qa-fullstack-app.onrender.com', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
